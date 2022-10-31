2e0c432ee7330fe33eafce5aa6b328a116be66ea
Garulf <535299+Garulf@users.noreply.github.com>
2022-10-31 03:43:48 -0400

    Fix mising len method

--- src/run.py
+++ src/run.py
@@ -4,7 +4,7 @@ from website import app
 from flask_frozen import Freezer
 
 if __name__ == '__main__':
-    if sys.argv > 1 and sys.argv[1] == 'build':
+    if len(sys.argv) > 1 and sys.argv[1] == 'build':
         freezer = Freezer(app)
         freezer.freeze()
     else:
